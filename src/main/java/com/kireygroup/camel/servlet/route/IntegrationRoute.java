6049cf3fb7c8b0d9a9addaae87f1eb513b046ebd
Francesco Spampinato <fspampinato@bridgeconsulting.it>
2026-02-08 18:32:56 +0100

    update dependencies and versions in pom.xml; remove unused HttpServletRequest import in IntegrationRoute.java

--- src/main/java/com/kireygroup/camel/servlet/route/IntegrationRoute.java
+++ src/main/java/com/kireygroup/camel/servlet/route/IntegrationRoute.java
@@ -5,7 +5,6 @@ import org.apache.camel.Message;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,8 +19,6 @@ public class IntegrationRoute extends EndpointRouteBuilder {
 				Message message = exchange.getMessage();
 		        String contentType = message.getHeader(Exchange.CONTENT_TYPE, String.class);
 		        String httpUri = message.getHeader(Exchange.HTTP_URI, String.class);
-		        HttpServletRequest req = message.getHeader(Exchange.HTTP_SERVLET_REQUEST, HttpServletRequest.class);
-		        log.info("http request {}", req);
 		        message.setBody("<b>Got Content-Type: " + contentType + ", URI: " + httpUri + "</b>");
 		        message.setHeader(Exchange.CONTENT_TYPE, "text/plain");
 			});
